a83b2f38c13d9d1e6e54cd0d80954874c15ead3e
jesusjimeneztapia <jesusjimeneztapia456@gmail.com>
2023-08-27 10:32:20 -0400

    Agrandar los tokens

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -15,7 +15,7 @@ export async function POST(request: Request) {
   const { messages } = await request.json()
 
   const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
+    model: 'gpt-3.5-turbo-16k',
     stream: true,
     messages: [
       {
